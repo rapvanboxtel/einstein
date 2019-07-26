a22c3bb665e08300da03b873be760c10d4fa8438
Rick van Boxtel <rapvanboxtel@gmail.com>
2019-07-26 16:19:16 +0200

    Rework Home (v0.5) and Scream (Dialog)

--- src/components/scream/Scream.js
+++ src/components/scream/Scream.js
@@ -18,9 +18,14 @@ import LikeButton from "./LikeButton";
 // Import Material UI library for styling the application
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
+
+import CardHeader from "@material-ui/core/CardHeader";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import CardActions from "@material-ui/core/CardActions";
 
 // Import Material UI library for icons
 import ChatIcon from "@material-ui/icons/Chat";
@@ -29,17 +34,12 @@ import ChatIcon from "@material-ui/icons/Chat";
 import { connect } from "react-redux";
 
 const styles = {
-  card: {
-    position: "relative",
-    display: "flex",
-    marginBottom: 20
+  cardContainer: {
+    width: "93%",
+    margin: "15px auto"
   },
-  image: {
-    minWidth: 200
-  },
-  content: {
-    padding: 25,
-    objectFit: "cover"
+  screamDialog: {
+    left: "85% !important"
   }
 };
 
@@ -69,39 +69,60 @@ class Scream extends Component {
       ) : null;
 
     return (
-      <Card className={classes.card}>
-        <CardMedia
-          image={userImage}
-          title="Profile image"
-          className={classes.image}
-        />
-        <CardContent className={classes.content}>
-          <Typography
-            variant="h5"
-            component={Link}
-            to={`/users/${userHandle}`}
-            color="primary"
-          >
-            {userHandle}
-          </Typography>
-          {deleteButton}
-          <Typography variant="body2" color="textSecondary">
-            {dayjs(createdAt).fromNow()}
-          </Typography>
-          <Typography variant="body1">{body}</Typography>
-          <LikeButton screamId={screamId} />
-          <span>{likeCount} Likes</span>
-          <MyButton tip="comments">
-            <ChatIcon color="primary" />
-          </MyButton>
-          <span>{commentCount} comments</span>
-          <ScreamDialog
-            screamId={screamId}
-            userHandle={userHandle}
-            openDialog={this.props.openDialog}
+      <div className={classes.cardContainer}>
+        <Card className={classes.card}>
+          <CardHeader
+            avatar={
+              <Avatar
+                alt="Profile image"
+                src={userImage}
+                className={classes.avatar}
+              />
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={
+              <Typography
+                variant="body2"
+                component={Link}
+                to={`/users/${userHandle}`}
+                color="primary"
+              >
+                {userHandle}
+              </Typography>
+            }
+            subheader={dayjs(createdAt).fromNow()}
           />
-        </CardContent>
-      </Card>
+
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {body}
+            </Typography>
+          </CardContent>
+
+          <CardActions disableSpacing>
+            <LikeButton screamId={screamId} />
+            <span>{likeCount} Likes</span>
+
+            <MyButton tip="comments">
+              <ChatIcon color="primary" />
+            </MyButton>
+            <span>{commentCount} comments</span>
+
+            <ScreamDialog
+              className={classes.screamDialog}
+              screamId={screamId}
+              userHandle={userHandle}
+              openDialog={this.props.openDialog}
+            />
+          </CardActions>
+
+          {/* {deleteButton} */}
+        </Card>
+      </div>
     );
   }
 }
--- src/components/scream/ScreamDialog.js
+++ src/components/scream/ScreamDialog.js
@@ -1,47 +1,47 @@
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
-import MyButton from '../../util/MyButton';
-import LikeButton from './LikeButton';
-import Comments from './Comments';
-import CommentForm from './CommentForm';
-import dayjs from 'dayjs';
-import { Link } from 'react-router-dom';
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+import MyButton from "../../util/MyButton";
+import LikeButton from "./LikeButton";
+import Comments from "./Comments";
+import CommentForm from "./CommentForm";
+import dayjs from "dayjs";
+import { Link } from "react-router-dom";
 // MUI Stuff
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 // Icons
-import CloseIcon from '@material-ui/icons/Close';
-import UnfoldMore from '@material-ui/icons/UnfoldMore';
-import ChatIcon from '@material-ui/icons/Chat';
+import CloseIcon from "@material-ui/icons/Close";
+import UnfoldMore from "@material-ui/icons/UnfoldMore";
+import ChatIcon from "@material-ui/icons/Chat";
 // Redux stuff
-import { connect } from 'react-redux';
-import { getScream, clearErrors } from '../../redux/actions/dataActions';
+import { connect } from "react-redux";
+import { getScream, clearErrors } from "../../redux/actions/dataActions";
 
-const styles = (theme) => ({
+const styles = theme => ({
   ...theme,
   profileImage: {
     maxWidth: 200,
     height: 200,
-    borderRadius: '50%',
-    objectFit: 'cover'
+    borderRadius: "50%",
+    objectFit: "cover"
   },
   dialogContent: {
     padding: 20
   },
   closeButton: {
-    position: 'absolute',
-    left: '90%'
+    position: "absolute",
+    left: "82.5%"
   },
   expandButton: {
-    position: 'absolute',
-    left: '90%'
+    position: "absolute",
+    left: "83%"
   },
   spinnerDiv: {
-    textAlign: 'center',
+    textAlign: "center",
     marginTop: 50,
     marginBottom: 50
   }
@@ -50,8 +50,8 @@ const styles = (theme) => ({
 class ScreamDialog extends Component {
   state = {
     open: false,
-    oldPath: '',
-    newPath: ''
+    oldPath: "",
+    newPath: ""
   };
   componentDidMount() {
     if (this.props.openDialog) {
@@ -113,7 +113,7 @@ class ScreamDialog extends Component {
           </Typography>
           <hr className={classes.invisibleSeparator} />
           <Typography variant="body2" color="textSecondary">
-            {dayjs(createdAt).format('h:mm a, MMMM DD YYYY')}
+            {dayjs(createdAt).format("h:mm a, MMMM DD YYYY")}
           </Typography>
           <hr className={classes.invisibleSeparator} />
           <Typography variant="body1">{body}</Typography>
@@ -169,7 +169,7 @@ ScreamDialog.propTypes = {
   UI: PropTypes.object.isRequired
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   scream: state.data.scream,
   UI: state.UI
 });
